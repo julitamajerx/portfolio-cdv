fcf58bdbe70b6278cf53b6ed365ecfb51e40a7b5
ScApeGoAT666 <julitamajer@gmail.com>
2026-01-09 17:25:48 +0100

    x

--- src/data.ts
+++ src/data.ts
@@ -3,21 +3,21 @@ import { Game } from "./games/game-model";
 export const sample_games: Game[] = [
   {
     id: 0,
-    img: 'game.jpeg',
+    img: 'pin-cover.png',
     title: 'Pin Away',
     date: 'July 7th 2023 - July 9th 2023',
     tag: 'GMTK Game Jam 2023',
   },
   {
     id: 1,
-    img: 'game.jpeg',
+    img: 'cats4.PNG',
     title: 'Cats Tower Defence',
     date: 'March 2025 - June 2025',
     tag: 'tower defence',
   },
   {
     id: 2,
-    img: 'game.jpeg',
+    img: 'mater6.png',
     title: 'Mater Dei',
     date: 'April 2024 - July 2024',
     tag: '2d platformer',
